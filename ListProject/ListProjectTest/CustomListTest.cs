492b86b20f7e0486dfb6659d23be1e363aef6099
unknown <jarydc333@gmail.com>
2019-03-12 11:34:44 -0500

    Unit Tests for the Add method are complete and checked off by an instructor. I will now add an indexer and begin working on the method

--- ListProject/ListProjectTest/CustomListTest.cs
+++ ListProject/ListProjectTest/CustomListTest.cs
@@ -8,8 +8,98 @@ namespace ListProjectTest
     public class CustomListTest
     {
         [TestMethod]
-        public void Add()
+        public void Add_EmptyArray_AddedToIndexZero()
         {
+            //Asign
+            CustomList<int> list = new CustomList<int>();
+            int addItem = 10;
+            int expected = 10;
+            int actual;
+
+            //Act
+            list.Add(addItem);
+            actual = list[0];
+
+            //Assert
+            Assert.AreEqual(expected,actual);
         }
+
+        [TestMethod]
+        public void Add_EmptyArray_CountIncrement()
+        {
+            //Asign
+            CustomList<int> list = new CustomList<int>();
+            int addItem = 1;
+            int expected = 1;
+            int actual;
+
+            //Act
+            list.Add(addItem);
+            actual = list.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddingFirstItem_CapacityIncrement()
+        {
+            //Asign
+            CustomList<int> list = new CustomList<int>();
+            int addItem = 1;
+            int expected = 4;
+            int actual;
+
+            //Act
+            list.Add(addItem);
+            actual = list.capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddingFifthItem_CapacityIncrement()
+        {
+            //Asign
+            CustomList<int> list = new CustomList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            int addItem = 1;
+            int expected = 8;
+            int actual;
+
+            //Act
+            list.Add(addItem);
+            actual = list.Capacity;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddingFifthItem_AddedToIndexFour()
+        {
+            //Asign
+            CustomList<int> list = new CustomList<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            int addItem = 10;
+            int expected = 10;
+            int actual;
+
+            //Act
+            list.Add(addItem);
+            actual = list[5];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
     }
 }
